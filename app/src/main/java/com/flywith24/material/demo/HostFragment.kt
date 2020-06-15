244099f80026394fcffd240a73e7ea9f25f3747d
fly_with24 <fly_with24@163.com>
2020-06-15 19:29:20 +0800

    use Host fragment

---
+++ app/src/main/java/com/flywith24/material/demo/HostFragment.kt
@@ -0,0 +1,13 @@
+package com.flywith24.material.demo
+
+import androidx.fragment.app.Fragment
+
+/**
+ * @author Flywith24
+ * @date   2020/6/15
+ * time   19:25
+ * description
+ */
+class HostFragment : Fragment(R.layout.fragment_host) {
+
+}
--- app/src/main/java/com/flywith24/material/demo/MainActivity.kt
+++ app/src/main/java/com/flywith24/material/demo/MainActivity.kt
@@ -3,9 +3,6 @@ package com.flywith24.material.demo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
 }
