622fa0b1885341ee221f40c71e193e4c222d4e2f
fly_with24 <fly_with24@163.com>
2020-06-15 20:03:38 +0800

    add HostBean.kt

---
+++ app/src/main/java/com/flywith24/material/demo/bean/HostBean.kt
@@ -0,0 +1,11 @@
+package com.flywith24.material.demo.bean
+
+import androidx.annotation.DrawableRes
+
+/**
+ * @author Flywith24
+ * @date   2020/6/15
+ * time   19:54
+ * description
+ */
+data class HostBean(val title: String, @DrawableRes val drawableResId: Int)
