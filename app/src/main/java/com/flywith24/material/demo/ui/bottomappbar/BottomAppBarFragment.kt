10ed9cc2ed8d98b3b01e56cfbc7f3986033b041d
fly_with24 <fly_with24@163.com>
2020-06-16 17:34:30 +0800

    add BottomAppBarFragment

---
+++ app/src/main/java/com/flywith24/material/demo/ui/bottomappbar/BottomAppBarFragment.kt
@@ -0,0 +1,22 @@
+package com.flywith24.material.demo.ui.bottomappbar
+
+import com.flywith24.material.demo.bean.Demo
+import com.flywith24.material.demo.ui.DemoLandingFragment
+
+/**
+ * @author Flywith24
+ * @date   2020/6/16
+ * time   16:50
+ * description
+ */
+class BottomAppBarFragment : DemoLandingFragment() {
+    override fun getDescription(): String = "BottomAppBarFragment"
+
+    override fun getDemos(): List<Demo> {
+        return super.getDemos()
+    }
+
+    override fun getAdditionalDemos(): List<Demo> {
+        return super.getAdditionalDemos()
+    }
+}
