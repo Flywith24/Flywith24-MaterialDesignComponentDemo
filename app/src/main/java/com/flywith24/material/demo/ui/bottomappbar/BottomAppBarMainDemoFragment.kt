497b8b4bfcef8d4fa9511717d4de3a6ce3e13791
fly_with24 <fly_with24@163.com>
2020-06-16 19:35:27 +0800

    BottomAppBarMainDemoFragment.kt

---
+++ app/src/main/java/com/flywith24/material/demo/ui/bottomappbar/BottomAppBarMainDemoFragment.kt
@@ -0,0 +1,12 @@
+package com.flywith24.material.demo.ui.bottomappbar
+
+import androidx.fragment.app.Fragment
+
+/**
+ * @author Flywith24
+ * @date   2020/6/16
+ * time   19:23
+ * description
+ */
+class BottomAppBarMainDemoFragment : Fragment() {
+}
