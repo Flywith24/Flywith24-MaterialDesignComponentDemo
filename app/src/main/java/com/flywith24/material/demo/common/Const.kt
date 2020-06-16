d58cd2662304f0f635598d647782c7d4fed2376b
fly_with24 <fly_with24@163.com>
2020-06-16 17:27:06 +0800

    add Const.kt

---
+++ app/src/main/java/com/flywith24/material/demo/common/Const.kt
@@ -0,0 +1,12 @@
+package com.flywith24.material.demo.common
+
+/**
+ * @author Flywith24
+ * @date   2020/6/16
+ * time   16:22
+ * description
+ */
+object Const {
+    const val TYPE_DEMO_TITLE = 1
+    const val TYPE_DEMO = 2
+}
